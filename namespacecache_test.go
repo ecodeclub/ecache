2ff0c8c803c7e22a608cca95b1a830b7f95197cd
wureny <wurenyu19@gmail.com>
2023-12-31 10:39:19 +0800

    更新了装饰器

--- namespacecache_test.go
+++ namespacecache_test.go
@@ -1,11 +1,26 @@
+// Copyright 2023 ecodeclub
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ecache
 
 import (
 	"context"
-	"github.com/ecodeclub/ekit"
-	"go.uber.org/mock/gomock"
 	"testing"
 	"time"
+
+	"github.com/ecodeclub/ekit"
+	"go.uber.org/mock/gomock"
 )
 
 // 测试namespace 是否真的正确处理了 key
@@ -23,7 +38,11 @@ func TestKeyChanged(t *testing.T) {
 		Err: nil,
 	}})
 
-	mockNamespaceCache.Set(context.Background(), key, "value", time.Duration(0))
+	err := mockNamespaceCache.Set(context.Background(), key, "value", time.Duration(0))
+	if err != nil {
+		t.Errorf("mockNamespaceCache.Set() error = %v, wantErr %v", err, nil)
+		return
+	}
 	v := mockNamespaceCache.Get(context.Background(), key)
 	if v.Err != nil {
 		t.Errorf("mockNamespaceCache.Get() error = %v, wantErr %v", v.Err, nil)
